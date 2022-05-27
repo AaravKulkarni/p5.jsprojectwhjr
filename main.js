a6604417c0c669b0d5a1b30a2a1c3a2db936fa1c
AaravKulkarni <88075592+AaravKulkarni@users.noreply.github.com>
2022-05-27 19:55:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload(){}
+function setup(){
+    canvas = createCanvas(700, 560);
+    canvas.position(500, 200);
+}
+function draw(){
+    circle(239, 234, 70);
+    rect(398, 458, 55, 90);
+    rect(500, 45, 67, 109, 20);
+    rect(108, 345, 90, 124, 20, 15, 10, 5);
+    ellipse(67, 89, 89, 138, 45);
+}
